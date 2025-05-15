36b0af4ed15f9136a940da8c427e7e898cfaf2bc
Alex <alexkiplangat22@gmail.com>
2025-05-15 16:48:08 +0300

    add

--- index.py
+++ index.py
@@ -1,19 +1,18 @@
 # Create an empty list
 my_list = []
 
-# Append elements to my_list
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
 
-# Insert the value 15 at the second position (index 1)
+
 my_list.insert(1, 15)
 
-# Extend my_list with another list [50, 60, 70]
+
 my_list.extend([50, 60, 70])
 
-# Remove the last element from my_list
+
 my_list.pop()
 
 # Sort my_list in ascending order
